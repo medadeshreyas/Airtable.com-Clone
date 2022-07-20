055be0ad2958888cb9ac767733c713b7de8d298e
Pushpendra Singh <90454357+erpushpendrasingh@users.noreply.github.com>
2022-07-20 14:29:11 +0530

    Footer Done (#8)

---
+++ component/footer.js
@@ -0,0 +1,231 @@
+const footer = () => {
+     return `<footer>
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid px-lg-5">
+                    <img class="navbar-brand" style="width: 32px"
+                        src="https://w7.pngwing.com/pngs/444/851/png-transparent-airtable-database-spreadsheet-logo-application-software-slack-logo-angle-rectangle-orange.png"
+                        alt="logo" />
+                    <div class="collapse navbar-collapse">
+                        <ul class="navbar-nav me-auto my-2 my-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link text-dark fw-semibold" href="#">Product</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark fw-semibold" href="#">Solutions</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark fw-semibold" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark fw-semibold">Enterprise</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark fw-semibold">Contact Sales</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div style="width: 200px" class="d-flex">
+                        <p class="m-2">
+                            <i style="font-size: 24px; color: rgb(165, 163, 163)" class="fa fa-facebook-square"></i>
+                        </p>
+
+                        <p class="m-2">
+                            <i style="font-size: 24px; color: rgb(165, 163, 163)" class="fa fa-linkedin-square"></i>
+                        </p>
+                        <p class="m-2">
+                            <i style="font-size: 24px; color: rgb(165, 163, 163)" class="fa fa-twitter"></i>
+                        </p>
+                        <p class="m-2">
+                            <i style="font-size: 24px; color: rgb(165, 163, 163)" class="fa fa-instagram"></i>
+                        </p>
+                        <p class="m-2">
+                            <i style="font-size: 24px; color: rgb(165, 163, 163)" class="fa fa-youtube-play"></i>
+                        </p>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="container-fluid border-top border-2 pt-5 mt-4" style="width: 93%">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 mb-4">
+                    <h6 class="mb-3 text-dark" style="letter-spacing: 2px">
+                        Popular Templates
+                    </h6>
+                    <ul class="list-unstyled mb-0">
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Content Calendar</a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Product Roadmap</a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Inventory Tracking</a>
+                        </li>
+
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Marketing Campaign
+                                Planner</a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Event Planning</a>
+                        </li>
+                    </ul>
+                    <h6 class="mt-3 text-dark" style="letter-spacing: 2px">
+                        Show more
+                    </h6>
+                </div>
+                <div class="col-lg-2 col-md-6 mb-4">
+                    <h6 class="mb-3 text-dark" style="letter-spacing: 2px">
+                        Popular extensions
+                    </h6>
+                    <ul class="list-unstyled mb-0">
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Miro</a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Jira</a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Clearbit</a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Loom</a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Formstack</a>
+                        </li>
+                    </ul>
+                    <h6 class="mt-3 text-dark" style="letter-spacing: 2px">
+                        Show more
+                    </h6>
+                </div>
+                <div class="col-lg-3 col-md-6 mb-4">
+                    <h6 class="mb-3 text-dark" style="letter-spacing: 2px">
+                        Popular Solutions
+                    </h6>
+                    <ul class="list-unstyled mb-0">
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Marketing
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Product Operations
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">
+                                Human Resources
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">
+                                Sales</a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Operations</a>
+                        </li>
+                    </ul>
+                    <h6 class="mt-3 text-dark" style="letter-spacing: 2px">
+                        Show more
+                    </h6>
+                </div>
+                <div class="col-lg-2 col-md-6 mb-4">
+                    <h6 class="mb-3 text-dark" style="letter-spacing: 2px">
+                        Learn More
+                    </h6>
+                    <ul class="list-unstyled mb-0">
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">
+                                Community
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">
+                                Support
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">
+                                Articles <span class="badge bg-success">New</span>
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">
+                                Guides <span class="badge bg-success">New</span>
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">
+                                Customer Stories
+                                <span class="badge bg-success">New</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-6 mb-4">
+                    <h6 class="mb-3 text-dark" style="letter-spacing: 2px; color: #7f4722">
+                        Company
+                    </h6>
+                    <ul class="list-unstyled mb-0">
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">About
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Careers
+                                <span class="badge bg-success">POPULAR</span>
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">
+                                Blog
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Status
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="#" class="text-decoration-none" style="color: #4f4f4f">Newsroom
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="text-center mt-5">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid py-2 px-lg-5 bg-light">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="#">Security</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="#">API</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold" href="#">Sitemap</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold">Privacy</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fw-semibold">Terms</a>
+                        </li>
+                    </ul>
+                    <div>
+                        <img style="width: 110px"
+                            src="https://www.freshtohome.com/skin/frontend/default/theme659/images/appstore_badge.png"
+                            alt="" />
+                        <img style="width: 110px"
+                            src="https://www.freshtohome.com/skin/frontend/default/theme659/images/playstore_badge.png"
+                            alt="" />
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </footer>`;
+};
+export { footer };
