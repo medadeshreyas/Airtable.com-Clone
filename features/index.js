234c855ac4206e5f4c887eacb358c9c88fbb577e
Atif Naquee <naqueeatif7@gmail.com>
2022-07-23 11:20:53 +0530

    atif

---
+++ features/index.js
@@ -0,0 +1,12 @@
+const clip =document.querySelectorAll('video');
+
+for(let i=0;i<clip.length;i++){
+    clip[i].addEventListener('mouseenter',function(e){
+        clip[i].play()
+    })
+    for(let i=0;i<clip.length;i++){
+        clip[i].addEventListener('mouseout',function(e){
+            clip[i].pause()
+        })
+}
+}
