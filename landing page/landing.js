07313374afe714667e887ab529d4a1f395028b68
medadeshreyas <shreyasmedade06@gmail.com>
2022-07-23 15:28:54 +0530

    Pushed temp landing page for pushparanjan
    Dont pull this except pushparanjan

---
+++ landing page/landing.js
@@ -0,0 +1,8 @@
+// document.getElementsByClassName("btn").addEventListener("click", myFun);
+function myFun() {
+  alert(
+    "This site is a demo version , You will be redirected to main website "
+  );
+  location.href =
+    "https://airtable.com/app9VXautCON2Gca5/tblyb8FW5gEwmWTq8/viwChbjqt6um8D6R6?blocks=hide";
+}
